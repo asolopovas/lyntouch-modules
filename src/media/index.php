bee08766c82f5173ca139e4b3a1427de47287b06
Andrius Solopovas <andrius.solopovas@gmail.com>
2020-11-03 13:01:01 +0000

    save

--- src/media/index.php
+++ src/media/index.php
@@ -7,11 +7,21 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-Routes::map('media', function() {
-    $request = Request::createFromGlobals();
-    $server = ServerFactory::create([
-                                        'source' => wp_get_upload_dir()['basedir'],
-                                        'cache'  => wp_get_upload_dir()['basedir'].'/glide_cache',
-                                    ]);
-    $server->outputImage($request->get('image'), $request->get('args'));
-});
+//Routes::map('media', function() {
+//    $request = Request::createFromGlobals();
+//    $server = ServerFactory::create([
+//                                        'source' => wp_get_upload_dir()['basedir'],
+//                                        'cache'  => wp_get_upload_dir()['basedir'].'/glide_cache',
+//                                    ]);
+//    $server->outputImage($request->get('image'), $request->get('args'));
+//});
+
+function add_custom_feed() {
+    add_feed( 'custom', 'render_custom_feed' );
+}
+add_action( 'init', 'add_custom_feed' );
+
+function render_custom_feed() {
+    header( 'Content-Type: text/html' );
+    echo 'aye!';
+}
