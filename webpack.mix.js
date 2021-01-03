dbcfc0181945f6c4c11871adea5b9032d5f3d402
Andrius Solopovas <andrius.solopovas@gmail.com>
2021-01-03 22:10:45 +0000

    save

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,9 @@
 let mix = require('laravel-mix');
 
-require('laravel-mix-tailwind');
 
-mix.react('src/Blocks/Image/index.js', 'dist/js')
+mix.js('src/Blocks/Image/index.js', 'dist/js').react()
    .sass('src/Blocks/Image/style.scss', 'dist/css')
    .sass('src/assets/admin/global.scss', 'dist/css')
    .sass('src/Blocks/Image/editor.scss', 'dist/css')
    .js('src/Blocks/Swiper/js/swiper.js', 'dist/js')
    .sass('src/Blocks/Swiper/sass/swiper.scss', 'dist/css')
-   .tailwind()
