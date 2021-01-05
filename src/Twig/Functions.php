c7c370e6446ba6d1573a33d849e58a3bf14d9c87
Andrius Solopovas <andrius.solopovas@gmail.com>
2021-01-05 23:49:55 +0000

    save

--- src/Twig/Functions.php
+++ src/Twig/Functions.php
@@ -12,14 +12,14 @@ class Functions
         return new Media($url, $width, $height, $format);
     }
 
-    public function srcsetGen(string $url, int $width, int $height, array $dpr, $format = 'jpg'): string
+    public function srcset(string $url, int $width, int $height, array $dpr, $format = 'jpg'): string
     {
         $output = [];
         foreach ($dpr as $key => $value) {
             $multiplier = $key+1;
             $str = $this->resize($url, $width *$multiplier, $height*$multiplier, $format);
 
-            $output[$key] = $key == 0 ? "$str" : "$str {$value}x";
+            $output[$key] = $key == 0 ? "$str" : "$str {$multiplier}x";
         }
 
         return implode(', ', $output);
