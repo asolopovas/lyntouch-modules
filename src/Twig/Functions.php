c4920402427afae3e20f2ef229cc15f976cc64ed
Andrius Solopovas <andrius.solopovas@gmail.com>
2021-01-10 23:10:22 +0000

    save

--- src/Twig/Functions.php
+++ src/Twig/Functions.php
@@ -8,6 +8,29 @@ use Lyntouch\Lib\Media;
 class Functions
 {
 
+    public function env($value)
+    {
+        return env($value);
+    }
+
+    public function build($name)
+    {
+        return build($name);
+    }
+
+    public function dump($value)
+    {
+        dump($value);
+    }
+
+    public function placeholder($width, $height, $color = '#cac9ca'): string
+    {
+        [$r, $g, $b] = sscanf($color, "#%02x%02x%02x");
+        $out = "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='{$width}' height='{$height}' viewBox='0 0 {$width} {$height}'><rect width='100%' height='100%' fill='rgb({$r}, {$g}, {$b})' /></svg>";
+
+        return str_replace('>', '%3E', str_replace('<', '%3C', $out));
+    }
+
     public function resize($url, $width, $height, $format = 'jpg'): Media
     {
         return new Media($url, $width, $height, $format);
@@ -29,5 +52,7 @@ class Functions
             file_put_contents('/tmp/error-wp.log', 'Error');
         }
     }
+
+
 
 }
